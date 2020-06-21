15476c3bd0d9183786ad587bfab6c91d2ce5ec4e
Mrigank  Anand <mrigank.rkee@rediffmail.com>
2020-06-21 16:56:11 +0530

    executing Multiple-linear-regression model

--- src/components/Algorithms/Linear-regression.js
+++ src/components/Algorithms/Linear-regression.js
@@ -7,7 +7,7 @@ class linearregression extends Component {
     state = {
         result: null,
         showresult: false,
-        x: 0,
+        x: null,
         error: null
     }
 
@@ -16,32 +16,55 @@ class linearregression extends Component {
         this.setState({x: x})
     }
 
-    result = () => {
-        const a = this.state.x
+    result = (event) => {
+        event.preventDefault();
+        const a = this.state.x;
         const data = {
             x: parseInt(a)
         }
-        axios.post("/linear-regression", data).then(response => {
-            this.setState({result: response.data})
-            this.setState({showresult: true})
-        }).catch(err => {
-            this.setState({errpr: "There is some error please try again"})
-        })
+        if (a !== null && a >= 0) {
+            axios.post("/linear-regression", data).then(response => {
+                this.setState({result: response.data})
+                this.setState({showresult: true})
+                this.setState({error: null})
+            })
+        } else {
+            this.setState({error: "Input is not valid it must be greater than or equal to zero"})
+        }
     }
 
 
     render() {
+        const style = {
+            border: "1px solid grey",
+            borderRadius: "20px",
+            padding: "1rem",
+            marginTop: "4rem"
+        }
         var prediction = null;
         if (this.state.showresult && !this.state.error) {
             prediction = <div>prediction for your case is : {this.state.result.result.toFixed(0)} dollars</div>;
         }
         return (
             <React.Fragment>
-                <h1>Linear regression</h1>
-                <Label>Enter no. of years of experience</Label>
-                <Input onChange={this.onchange} placeholder={"No. of years"}/>
-                <Button onClick={this.result}>Click to get result</Button>
-                {prediction}
+                <div className="ui container left aligned" style={style}>
+                    <h1 className={"centered"}>Linear Regression</h1>
+                    <div className="ui section divider"></div>
+                    <form className="ui form" onSubmit={this.result}>
+                        <div className="field">
+                            <label>Enter no. of years of experience</label>
+                            <input type="number" min="0" onChange={this.onchange} placeholder={"No. of years"}
+                                   required/>
+                        </div>
+                        <button type={"submit"} className="ui button">Click to get result</button>
+                    </form>
+                    {/*<div style={{margin: "10px 0px"}}>*/}
+                    {/*</div>*/}
+                    <div>
+                        {prediction}
+                        {this.state.error}
+                    </div>
+                </div>
             </React.Fragment>
         );
     }
--- src/components/Algorithms/Multiple-linear.js
+++ src/components/Algorithms/Multiple-linear.js
@@ -10,38 +10,58 @@ class multipleLinear extends Component {
         sop: 1,
         lor: 1,
         cgpa: 6.8,
+        research: true,
         result: null,
         showresult: null,
-        error: null
+        error: null,
+        min: {
+            cgpa: 6.8,
+            grescore: 290,
+            lor: 1,
+            sop: 1,
+            toeflscore: 92,
+            universityrating: 1,
+        },
+        max: {
+            cgpa: 9.92,
+            grescore: 340,
+            lor: 5,
+            sop: 5,
+            toeflscore: 120,
+            universityrating: 5
+        }
     }
+
+    // componentDidMount() {
+    //     axios.get("/multiplelinearregression").then(response => {
+    //         console.log(response.data.data.min)
+    //         console.log(response.data.data.max);
+    //     }).catch(err => {
+    //         console.log(err)
+    //     })
+    // }
 
     onchange = (event, name) => {
         const variable = event.target.value
         this.setState({name: variable})
     }
 
-    result = () => {
+    result = (event) => {
+        event.preventDefault()
         const data = {
-            // grescore: parseInt(this.state.grescore),
-            // toeflscore: parseInt(this.state.toeflscore),
-            // universityrating: parseInt(this.state.universityrating),
-            // sop: parseFloat(this.state.sop),
-            // lor: parseFloat(this.state.lor),
-            // cgpa: parseFloat(this.state.cgpa),
-            // research: parseInt(1)
-            "grescore": 290,
-            "toeflscore": 2,
-            "universityrating": 1,
-            "sop": 1,
-            "lor": 1,
-            "cgpa": 6.8,
-            "research": 1
+            grescore: parseInt(this.state.grescore),
+            toeflscore: parseInt(this.state.toeflscore),
+            universityrating: parseInt(this.state.universityrating),
+            sop: parseFloat(this.state.sop),
+            lor: parseFloat(this.state.lor),
+            cgpa: parseFloat(this.state.cgpa),
+            research: parseInt(this.state.research)
 
         }
         console.log(data)
         axios.post('/multiplelinearregression', data)
             .then(response => {
-                console.log(response)
+                console.log(response.data)
                 this.setState({
                     result: response.data,
                     showresult: true
@@ -54,53 +74,104 @@ class multipleLinear extends Component {
             })
         })
     }
+    grechangeHandler = (event) => {
+        this.setState({grescore: event.target.value})
+    }
+    toeflchangeHandler = (event) => {
+        this.setState({toeflscore: event.target.value})
+    }
+    sopchangeHandler = (event) => {
+        this.setState({sop: event.target.value})
+    }
+    lorchangeHandler = (event) => {
+        this.setState({lor: event.target.value})
+    }
+    universityrankingchangeHandler = (event) => {
+        this.setState({universityrating: event.target.value})
+    }
+    researchchangeHandler = (event) => {
+        const value = this.state.research
+        if (value === 1) {
+            this.setState({research: 0})
+        } else {
+            this.setState({research: 1})
+        }
+
+    }
+    cgpachangeHandler = (event) => {
+        this.setState({cgpa: event.target.value})
+    }
 
     render() {
+        let probability = null;
+        if (this.state.result) {
+            probability = (
+                <div>
+                    <div className="ui section divider"></div>
+                    <h3><strong>Probability of your admission is : {this.state.result.result}</strong></h3>
+                </div>
+            )
+        }
         const style = {
             border: "1px solid grey",
             borderRadius: "20px",
-            padding: "2rem",
-            marginTop: "1rem"
+            padding: "1rem",
+            marginTop: "4rem"
         }
         return (
             <React.Fragment>
                 <div className="ui container left aligned" style={style}>
                     <h1 className={"centered"}>Multiple Linear Regression</h1>
-                    <form className="ui form">
+                    <div className="ui section divider"></div>
+                    <form className="ui form" onSubmit={this.result} id={"form"}>
                         <div className="field">
                             <label>Gre score</label>
-                            <input type="number" name="grescore" required placeholder="Grescore 290 - 340"/>
+                            <input type="number" name="grescore" min={this.state.min.grescore}
+                                   onChange={this.grechangeHandler}
+                                   max={this.state.max.grescore} required placeholder="Grescore 290 - 340"/>
                         </div>
                         <div className="field">
                             <label>Toefl Score</label>
-                            <input type="number" name="toeflscore" placeholder="Toefl-Score" required/>
+                            <input type="number" name="toeflscore" min={this.state.min.toeflscore}
+                                   onChange={this.toeflchangeHandler}
+                                   max={this.state.max.toeflscore} placeholder="Toefl-Score" required/>
                         </div>
                         <div className="field">
                             <label>University Rating</label>
-                            <input type="number" name="grescore" required placeholder="university rating" required/>
+                            <input type="number" name="university rating" required min={this.state.min.universityrating}
+                                   onChange={this.universityrankingchangeHandler}
+                                   max={this.state.max.universityrating} placeholder="university rating" required/>
                         </div>
                         <div className="field">
-                            <label>sop</label>
-                            <input type="number" name="grescore" required placeholder="sop" required/>
+                            <label>Sop</label>
+                            <input type="number" name="sop" required min={this.state.min.sop} max={this.state.max.sop}
+                                   onChange={this.sopchangeHandler}
+                                   placeholder="sop" required/>
                         </div>
                         <div className="field">
-                            <label>lor</label>
-                            <input type="number" name="toeflscore" placeholder="lor" required/>
+                            <label>Lor</label>
+                            <input type="number" name="lor" min={this.state.min.lor} max={this.state.max.lor}
+                                   onChange={this.lorchangeHandler}
+                                   placeholder="lor" required/>
                         </div>
                         <div className="field">
                             <label>Cgpa</label>
-                            <input type="number" name="toeflscore" placeholder="cgpa" required/>
+                            <input type="number" min={this.state.min.cgpa} max={this.state.max.cgpa} name="cgpa"
+                                   placeholder="cgpa" required onChange={this.cgpachangeHandler}/>
                         </div>
                         <div className="ui segment">
                             <div className="field">
                                 <div className="ui toggle checkbox">
-                                    <input type="checkbox" name="gift" tabIndex="0" className="hidden"/>
-                                    <label>Do not include a receipt in the package</label>
+                                    <input type="checkbox" name="gift" tabIndex="0"
+                                           onChange={this.researchchangeHandler}/>
+                                    <label>Research</label>
                                 </div>
                             </div>
                         </div>
                         <button className="ui button" type="submit">Submit</button>
                     </form>
+                    {probability}
+                    {/*{}*/}
                 </div>
             </React.Fragment>
         )
